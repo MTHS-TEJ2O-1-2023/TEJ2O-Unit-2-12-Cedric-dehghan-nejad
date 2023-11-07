7d6ebfced0c845b0b36162bcd5f3f4c958eee75c
Cedric dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-11-07 12:48:24 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -26,6 +26,7 @@ input.onButtonPressed(Button.A, function () {
     DigitalPin.P2,
     PingUnit.Centimeters
   )
+  // if distanceToObject is less than 10 than light up Red 
   if (distanceToObject < 10) {
     basic.showNumber(distanceToObject)
     basic.showIcon(IconNames.Happy)
@@ -39,14 +40,15 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
     basic.showNumber(distanceToObject)
   } else 
-    { if (distanceToObject >= 10)
-      basic.showIcon(IconNames.Happy)
-    neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-    neopixelStrip.show()
+  // if distancetoobject is greater 10 light up Green
+   if (distanceToObject >= 10) {
+  basic.showIcon(IconNames.Happy)
+  neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+  neopixelStrip.show()
     basic.showNumber(distanceToObject)
     basic.showIcon(IconNames.Happy)
   }
