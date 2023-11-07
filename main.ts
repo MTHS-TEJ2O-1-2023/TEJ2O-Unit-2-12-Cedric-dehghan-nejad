e4ccbcc00ac24232fb8e3396ca3506dc0ea065e8
Cedric dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-11-06 21:14:06 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -26,7 +26,7 @@ input.onButtonPressed(Button.A, function () {
     DigitalPin.P2,
     PingUnit.Centimeters
   )
-  if (distanceToObject < 10 ){
+  if (distanceToObject < 10) {
     basic.showNumber(distanceToObject)
     basic.showIcon(IconNames.Happy)
     neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
@@ -39,7 +39,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
     basic.showNumber(distanceToObject)
   } else {
-     (distanceToObject >= 10)
+    (distanceToObject >= 10)
     basic.showIcon(IconNames.Happy)
     neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
