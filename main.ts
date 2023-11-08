809ab58909baaee6161b7c15048de18dc471e684
Cedric dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-11-08 12:57:56 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -28,23 +28,23 @@ input.onButtonPressed(Button.A, function () {
     DigitalPin.P2,
     PingUnit.Centimeters
     )
-    // output distance in cm
+  // output distance in cm
   basic.showNumber(distanceToObject)
   basic.showString('cm')
 
     // if distance is less than 10, turn all neopixels to red, else turn green
-    if (distanceToObject < 10) {
-      neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-      neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-      neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
-      neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
-      neopixelStrip.show()
-      basic.showIcon(IconNames.Yes)
+  if (distanceToObject < 10) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    neopixelStrip.show()
+    basic.showIcon(IconNames.Yes)
     } else {
-      neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-      neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-      neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-      neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-      neopixelStrip.show()
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
     }
 })
